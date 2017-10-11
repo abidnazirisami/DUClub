1fdbcb7118d8116bc6f404f5fa1a10b6dc17d3da
Abid Naziri Sami <abidnazirisami@gmail.com>
2017-10-11 22:12:01 +0600

    Version 1.0.2

---
+++ neweb/neweb/urls.py
@@ -0,0 +1,32 @@
+"""neweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from neweb.views import getPrice
+from neweb.views import getList
+from neweb.views import getWeeklyList
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^foodPrice/(.{1,30})', getPrice),
+    url(r'^foods/$', getList),
+    url(r'^foodList/(.{0,30})$', getWeeklyList),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+]
